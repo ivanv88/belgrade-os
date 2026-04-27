6444723c5d448857d7d16a35045120880c48373e
ivan <ivanv88@gmail.com>
2026-04-28 01:13:23 +0200

    fix: emit cargo:rerun-if-changed for proto file in bridge build.rs

--- bridge/build.rs
+++ bridge/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../proto/belgrade_os.proto");
     prost_build::compile_protos(
         &["../proto/belgrade_os.proto"],
         &["../proto"],
