7ba3b56652c84350873ae95748fca1e9b67d47b5
ivan <ivanv88@gmail.com>
2026-05-18 02:51:23 +0200

    chore: move bridge to services/bridge — fix build.rs proto path (../ → ../../)

--- bridge/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=../proto/belgrade_os.proto");
-    prost_build::compile_protos(
-        &["../proto/belgrade_os.proto"],
-        &["../proto"],
-    )?;
-    Ok(())
-}
